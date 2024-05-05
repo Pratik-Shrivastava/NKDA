7c98808881b6125bb0807c11033f51c54cfc005f
Deep-Dey <deepdey20998@gmail.com>
2024-05-05 19:53:12 +0530

    feat: add sanitizer as decorator

---
+++ app/decorators/sanitizer.py
@@ -0,0 +1,20 @@
+from functools import wraps
+from flask import request
+
+from app.utils.sanitizer import sanitize
+
+def sanitized():
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            arguments = request.args.to_dict()
+            request.args = sanitize(arguments)
+            try:
+                payload = request.get_json()
+                request.json = sanitize(payload)
+            except Exception as e:
+                pass
+
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
